391bfe15e6bd6699e1ba69d1b1144043a5be268d
Sam Pulaski <baconsamp@gmail.com>
2025-04-25 10:28:21 -0400

    removed extraneous routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Main from './main';
-import Quote from './components/quotes/quote.component';
-import Madlibs from './components/madlibs/madlibs.component';
 import { CommentsProvider } from './contexts/commentsContext';
 
 function App() {
